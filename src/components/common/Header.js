681daa7eee7ebe463c70dfa017f823d82c922360
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-24 22:21:17 +0530

    create header Component

---
+++ src/components/common/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+         <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="true">
+            <div className="uk-navbar-left">
+                <ul className="uk-navbar-nav">
+                    <li><Link to="/">Home</Link></li>
+                </ul>
+            </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from "react-router";
+import { BrowserRouter, Switch } from "react-router-dom";
+
 import App from './components/App';
+import Header from './components/common/Header';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={App}></Route>
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
