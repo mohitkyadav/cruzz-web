6ddc8264ebf2d91659fea7eaa2728e4d285aaab3
Yogender Jangir <201651054@iiitvadodara.ac.in>
2018-10-25 15:35:14 +0530

    create login page

--- src/components/common/Login.js
+++ src/components/common/Login.js
@@ -6,9 +6,22 @@ import '../../static/css/common.css';
 class Login extends Component {
   render() {
     return (
-      <div>
-        fd
-      </div>
+        <div class="loginForm">
+          <form>
+            <div class="uk-margin">
+              <div class="uk-inline">
+                <span class="uk-form-icon" uk-icon="icon: user" placeholder="Username"></span>
+                <input class="uk-input" type="text"/>
+              </div>
+              <div class="uk-margin">
+                <div class="uk-inline">
+                  <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                  <input class="uk-input" type="text" placeholder="Password"/>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div> 
     );
   }
 }
