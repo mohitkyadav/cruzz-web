c70cb4ebfb0404554d76af05542ddba4037d4e26
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-25 15:32:34 +0530

    update header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,19 +7,19 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <nav className="uk-navbar-container uk-box-shadow-large" uk-navbar="true" uk-sticky="show-on-up: true;animation: uk-animation-slide-top;">
+        <nav className="uk-navbar-container uk-box-shadow-large" uk-navbar="true" uk-sticky="show-on-up: true; animation: uk-animation-slide-top;">
           <div className="uk-navbar-left">
             <ul className="uk-navbar-nav">
-              <li><Link to="/">Home</Link></li>
+              <li><Link className="ov-color-white" to="/">Home</Link></li>
             </ul>
 
             <div>
-              <Link to="#" className="uk-navbar-toggle" uk-icon="icon: search"></Link>
-              <div className="uk-navbar-dropdown" uk-drop="mode: click; cls-drop: uk-navbar-dropdown; boundary: !nav">
+              <Link to="#" className="uk-navbar-toggle ov-color-white" uk-icon="icon: search"></Link>
+              <div className="uk-navbar-dropdown uk-padding-small uk-width-large" uk-drop="mode: click; cls-drop: uk-navbar-dropdown; boundary: !nav; animation: uk-animation-slide-left-small; reverse:  true;">
 
                 <div className="uk-grid-small uk-flex-middle" uk-grid>
                   <div className="uk-width-expand">
-                    <form className="uk-search uk-search-navbar uk-width-1-1">
+                    <form className="uk-search uk-search-small uk-search-navbar uk-width-1-1">
                       <input className="uk-search-input" type="search" placeholder="Search..." autofocus/>
                     </form>
                   </div>
@@ -31,10 +31,10 @@ class Header extends Component {
           <div className="uk-navbar-right uk-margin-small-right">
             <ul className="uk-navbar-nav">
               <ul className="uk-iconnav">
-                <li><Link to="#" uk-icon="icon: home; ratio: 1.2" uk-tooltip="title: Home; pos: bottom-center"></Link></li>
-                <li><Link to="#" uk-icon="icon: bell; ratio: 1.2" uk-tooltip="title: Notifications; pos: bottom-center"></Link></li>
-                <li><Link to="#" uk-icon="icon: user; ratio: 1.2" uk-tooltip="title: Profile; pos: bottom-center"></Link></li>
-                <li><Link to="#" uk-icon="icon: cog; ratio: 1.2" uk-tooltip="title: Settings; pos: bottom-center"></Link></li>
+                <li><Link className="ov-color-white" to="#" uk-icon="icon: home; ratio: 1.2" uk-tooltip="title: Home; pos: bottom-center"></Link></li>
+                <li><Link className="ov-color-white" to="#" uk-icon="icon: bell; ratio: 1.2" uk-tooltip="title: Notifications; pos: bottom-center"></Link></li>
+                <li><Link className="ov-color-white" to="#" uk-icon="icon: user; ratio: 1.2" uk-tooltip="title: Profile; pos: bottom-center"></Link></li>
+                <li><Link className="ov-color-white" to="#" uk-icon="icon: cog; ratio: 1.2" uk-tooltip="title: Settings; pos: bottom-center"></Link></li>
               </ul>
             </ul>
           </div>
