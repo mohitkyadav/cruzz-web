7d432538d9a505dac8a964b3251f51652ecbd4ba
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-25 16:30:05 +0530

    hover on nav items

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -10,16 +10,16 @@ class Header extends Component {
         <nav className="uk-navbar-container uk-box-shadow-large" uk-navbar="true" uk-sticky="show-on-up: true; animation: uk-animation-slide-top;">
           <div className="uk-navbar-left">
             <ul className="uk-navbar-nav">
-              <li><Link className="ov-color-white" to="/">Home</Link></li>
+              <li><Link className="ov-color-white ov-nav-link" to="/"><span uk-icon="icon: home; ratio: 1.15"></span></Link></li>
             </ul>
 
             <div>
-              <Link to="#" className="uk-navbar-toggle ov-color-white" uk-icon="icon: search"></Link>
-              <div className="uk-navbar-dropdown uk-padding-small uk-width-large" uk-drop="mode: click; cls-drop: uk-navbar-dropdown; boundary: !nav; animation: uk-animation-slide-left-small; reverse:  true;">
+              <Link to="#" className="uk-navbar-toggle ov-color-white ov-nav-link" uk-icon="icon: search"></Link>
+              <div className="uk-navbar-dropdown uk-padding-small uk-width-large@m uk-width-medium" uk-drop="mode: click; cls-drop: uk-navbar-dropdown; boundary: !nav; animation: uk-animation-slide-left-small;">
 
                 <div className="uk-grid-small uk-flex-middle" uk-grid>
                   <div className="uk-width-expand">
-                    <form className="uk-search uk-search-small uk-search-navbar uk-width-1-1">
+                    <form className="uk-search uk-search-navbar uk-width-1-1">
                       <input className="uk-search-input" type="search" placeholder="Search..." autofocus/>
                     </form>
                   </div>
@@ -28,14 +28,11 @@ class Header extends Component {
             </div>
           </div>
 
-          <div className="uk-navbar-right uk-margin-small-right">
+          <div className="uk-navbar-right">
             <ul className="uk-navbar-nav">
-              <ul className="uk-iconnav">
-                <li><Link className="ov-color-white" to="#" uk-icon="icon: home; ratio: 1.2" uk-tooltip="title: Home; pos: bottom-center"></Link></li>
-                <li><Link className="ov-color-white" to="#" uk-icon="icon: bell; ratio: 1.2" uk-tooltip="title: Notifications; pos: bottom-center"></Link></li>
-                <li><Link className="ov-color-white" to="#" uk-icon="icon: user; ratio: 1.2" uk-tooltip="title: Profile; pos: bottom-center"></Link></li>
-                <li><Link className="ov-color-white" to="#" uk-icon="icon: cog; ratio: 1.2" uk-tooltip="title: Settings; pos: bottom-center"></Link></li>
-              </ul>
+              <li><Link className="ov-color-white ov-nav-link" to="#" uk-icon="icon: bell; ratio: 1.2" uk-tooltip="title: Notifications; pos: bottom-center"></Link></li>
+              <li><Link className="ov-color-white ov-nav-link" to="#" uk-icon="icon: user; ratio: 1.2" uk-tooltip="title: Profile; pos: bottom-center"></Link></li>
+              <li><Link className="ov-color-white ov-nav-link" to="#" uk-icon="icon: cog; ratio: 1.2" uk-tooltip="title: Settings; pos: bottom-center"></Link></li>
             </ul>
           </div>
         </nav>
