9cd0a3fe951dbf5a4e707a4ece435143204cd7f6
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-10-27 14:22:19 +0530

    push offcanvas

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,18 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="loading" alt="logo" />
           <h1 className="App-title">Welcome aboard!</h1>
+          <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-push">Push</button>
+          <div id="offcanvas-push" uk-offcanvas="mode: push; overlay: true">
+          <div class="uk-offcanvas-bar">
+
+            <button class="uk-offcanvas-close" type="button" uk-close></button>
+
+            <h3>Title</h3>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+           </div>
+          </div>
         </header>
       </div>
     );
