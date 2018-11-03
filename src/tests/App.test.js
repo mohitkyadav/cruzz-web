94ffeac8e9c2af1ccc8523a915badddd6ac4e8e6
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-11-03 15:30:10 +0530

    test with
    router tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router'
+
 import App from '../components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(withRouter(<App />), div);
   ReactDOM.unmountComponentAtNode(div);
 });
