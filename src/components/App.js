0daab2a29f1dd36b918f4714c5d4f1b687787c27
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-11-09 19:11:37 +0530

    feed 🙌🙌🙌🙌🎉🎉🎉

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,37 @@ import { withRouter } from 'react-router-dom';
 
 import '../static/css/style.css';
 import logo from '../static/img/index.svg';
+import { loading, loaded } from './../actions/authActions';
+import axios from 'axios';
+import PostFeed from './feed/PostFeed';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: {}
+    }
+    this.fetchPosts = this.fetchPosts.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  fetchPosts() {
+    axios.get('https://cruzz.herokuapp.com/api/post/feed/?&limit=100&offset=0/')
+    .then(res => {
+      this.setState({
+        posts: res.data.posts.reverse()
+      });
+      console.log(res.data.posts)
+    }).catch(err => {
+      console.log(err.response);
+    });
+  }
+
   render() {
     let spinner;
 
@@ -22,6 +50,29 @@ class App extends Component {
         <div className="uk-align-center uk-height-1-1 uk-width-1-1">
           {spinner}
         </div>
+        <div data-uk-grid="true">
+          <div className="uk-width-4-5@m uk-align-center">
+          {
+            this.state.posts.length > 0 ? (
+              <div>
+                {this.state.posts.map((post, key) => {
+                  return (
+                    <div key={key}>
+                      <PostFeed post={post} full={false}/>
+                    </div>
+                  )
+                })}
+              </div>
+            ):(
+              <div>
+                <div>
+                  <img alt="loading" className="uk-align-center" src={spinner}></img>
+                </div>
+              </div>
+            )
+          }
+          </div>
+        </div>
       </div>
     );
   }
@@ -31,4 +82,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps)(withRouter(App));
+export default connect(mapStateToProps, {loading, loaded})(withRouter(App));
